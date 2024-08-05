aa6cca157a2576dae4a9f78b7b34884ab9630df1
izeng0975 <iz2133@nyu.edu>
2024-08-04 22:36:01 -0400

    first commit

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAryRlD-ZBNT7TIB1RBaFsKGHFj1AZLMsw",
+  authDomain: "pantry-tracking-ef9f9.firebaseapp.com",
+  projectId: "pantry-tracking-ef9f9",
+  storageBucket: "pantry-tracking-ef9f9.appspot.com",
+  messagingSenderId: "993907357094",
+  appId: "1:993907357094:web:46dcf3111af0478327cb67",
+  measurementId: "G-0NB57C8VES",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore };
